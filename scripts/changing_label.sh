663398d7fc7d4d2c9bccf2784faa8ba0bdc18896
Dennis Angemi <dennisangemi@gmail.com>
2022-01-16 13:17:21 +0100

    added script #17

---
+++ scripts/changing_label.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script will change the label of the fields '%immunizzati' and '%vaccinati' of every csv file in the dati/vaccini directory
+
+# Test su singolo file. Funziona!
+# sed 's/%immunizzati/%seconda_dose/' ../dati/vaccini/vaccini-latest.csv > ../dati/vaccini/test.csv
+
+# Rename '%vaccinati' to '%prima_dose' and '%immunizzati' to '%seconda_dose'
+for file in ../dati/vaccini/*.csv
+do
+    echo "Processing $file"
+    sed 's/%vaccinati/%prima_dose/' $file > ${file}_new.csv
+    mv ${file}_new.csv $file
+    sed 's/%immunizzati/%seconda_dose/' $file > ${file}_new.csv
+    mv ${file}_new.csv $file
+done
