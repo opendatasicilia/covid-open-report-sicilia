026ce4d63810a9fad4548f92ddab1f583aca9cd7
Gabriele Scalici <gaba41@gmail.com>
2022-04-17 04:40:39 +0200

    update ocr algo

--- scripts/convertV2.py
+++ scripts/convertV2.py
@@ -1,8 +1,13 @@
+import io
 import fitz
+import numpy as np
+from PIL import Image, ImageOps, ImageFilter, ImageEnhance
+import pytesseract
 import pandas as pd
+from difflib import SequenceMatcher
 
 path = '.'
-comuni = pd.DataFrame(pd.read_csv(
+comuni_siciliani = pd.DataFrame(pd.read_csv(
     'https://raw.githubusercontent.com/gabacode/vaxExtract/main/utilities/Elenco-comuni-siciliani.csv', converters={'pro_com_t': '{:0>6}'.format}))
 latest = pd.DataFrame(pd.read_csv(path+'/download/report.csv')).iloc[-1]
 file = latest['nome_file']
@@ -36,6 +41,36 @@ def isDigit(x):
         return True
     except ValueError:
         return False
+
+
+def similar(a, b):
+    '''Check how similar are two strings'''
+    return SequenceMatcher(None, a, b).ratio()
+
+
+def resize(image):
+    '''Resize all pages to the same size'''
+    invert_im = image.convert("RGB")
+    invert_im = ImageOps.invert(invert_im)
+    imageBox = invert_im.getbbox()
+    cropped = image.crop(imageBox)
+    aspect_ratio = cropped.height / cropped.width
+    new_width = 2121
+    new_height = int(new_width * aspect_ratio)
+    resized = cropped.resize((new_width, new_height), Image.NEAREST)
+    resized_image = np.array(resized)
+    return resized_image
+
+
+def prepare(image, mode):
+    '''Process Images to make them more readable'''
+    image = image.convert('L')
+    image = image.filter(ImageFilter.SMOOTH_MORE)
+    if mode == 'numbers':
+        image = ImageOps.invert(image)
+    brightness = ImageEnhance.Brightness(image)
+    image = brightness.enhance(1.2)
+    return image
 
 
 def getIncidenza(file, pages):
@@ -87,8 +122,8 @@ def getIncidenza(file, pages):
     incidenza.reset_index(inplace=True, drop=True)
 
     # Inner join e recupera info comuni
-    out = pd.merge(incidenza, comuni, left_on=incidenza["comune"].str.lower(
-    ), right_on=comuni["comune"].str.lower(), how="inner")
+    out = pd.merge(incidenza, comuni_siciliani, left_on=incidenza["comune"].str.lower(
+    ), right_on=comuni_siciliani["comune"].str.lower(), how="inner")
     out.rename(columns={'comune_y': 'comune'}, inplace=True)
     out = out[['cod_prov', 'pro_com_t', 'provincia', 'comune',
                'incidenza', 'casi']].sort_values(by=['provincia', 'comune'])
@@ -116,41 +151,98 @@ def getIncidenza(file, pages):
 
 def getVax(file, pages):
     print('Leggo tabella Vaccini...attendi...')
+    zoom = 4
+    mat = fitz.Matrix(zoom, zoom)
+    letters_config = r'--oem 3 --psm 12 -c tessedit_char_blacklist=[]()!'
+    numbers_config = r'--oem 3 --psm 12 -c tessedit_char_whitelist=0123456789,%'
+    report = pd.DataFrame(pd.read_csv(
+        'https://raw.githubusercontent.com/opendatasicilia/covid-open-report-sicilia/main/dati/vaccini/vaccini-latest.csv', converters={'pro_com_t': '{:0>6}'.format}))
+    comuni = []
+    values = []
+    lista_comuni = report['comune'].tolist()
+    pdf = fitz.open(file)
+    for index in range(pages[0], pages[-1]):
+        page = pdf.load_page(index)
+        pixmap = page.get_pixmap(alpha=False, matrix=mat).tobytes()
+        image = Image.open(io.BytesIO(pixmap))
+        resized = resize(image)
 
-    with open('vax.txt') as inputFile:
-        lines = inputFile.read().splitlines()
-        it = iter(lines)
-        data = list(zip(it, it, it, it, it, it, it))
+        # Colonna Comuni
+        comuni_column = resized[730:image.height*2, 230:620]
+        comuni_column_image = Image.fromarray(comuni_column)
+        processed_comuni = prepare(comuni_column_image, 'names')
+        results = pytesseract.image_to_string(
+            processed_comuni, config=letters_config).split('\n')
+        lista_risultati = [i for i in results if i]
+        lista_risultati.remove('\x0c')
+        for item in lista_risultati:
+            for index, comune in enumerate(lista_comuni):
+                if similar(item, comune) == 1:
+                    comuni.append(comune)
+                    del lista_comuni[index]
+                    break
+                elif similar(item, comune) > 0.85:
+                    del lista_comuni[index]
+                    comuni.append(comune)
+                    break
+                elif similar(item, comune) > 0.66:
+                    del lista_comuni[index]
+                    comuni.append(comune)
+                    break
+                elif similar(item, comune) > 0.57:
+                    del lista_comuni[index]
+                    comuni.append(comune)
+                    break
+                else:
+                    continue
 
-        vax = pd.DataFrame(
-            data, columns=['comune', '51', '52', '121', '122', 'prima_dose', 'seconda_dose'])
-        vax = vax[['comune', 'prima_dose', 'seconda_dose']]
-        vax['prima_dose'] = vax['prima_dose'].str.replace(
-            ',', '.').str.rstrip('%')
-        vax['seconda_dose'] = vax['seconda_dose'].str.replace(
-            ',', '.').str.rstrip('%')
-        out = pd.merge(vax, comuni, on='comune', how='inner')
-        out = out[['cod_prov', 'pro_com_t', 'provincia',
-                   'comune', 'prima_dose', 'seconda_dose']]
-        out.insert(0, 'data', date)
-        assert (len(out) == 390), "Errore: Sono presenti meno comuni del previsto."
-        print(out)
+        # Colonna Numeri
+        numbers_column = resized[730:image.height*2, 1500:1900]
+        numbers_column_image = Image.fromarray(numbers_column)
+        processed_numbers = prepare(numbers_column_image, 'numbers')
+        results = pytesseract.image_to_string(
+            processed_numbers, config=numbers_config).split('\n')
 
-        lastUpdate = open(
-            path+'/dati/vaccini/vaccini.csv').read().rsplit('\n', 2)[1].split(',')[0]
-        if date != lastUpdate:
-            print('Esporto CSV...')
-            out.to_csv(path+'/dati/vaccini/vaccini-' +
-                       date.replace("-", "")+'.csv', index=None, header=True)
-            out.to_csv(path+'/dati/vaccini/vaccini-latest.csv',
-                       index=None, header=True)
-            out.to_csv(path+'/dati/vaccini/vaccini.csv',
-                       mode='a', index=None, header=False)
-        csv = path+'/dati/vaccini/vaccini-'+date.replace("-", "")+'.csv'
+        out = [i for i in results if i]
+        out.remove('\x0c')
+        out = [value.replace(',', '.').replace('%', '') for value in out]
+        out = out[:-2]
 
-        return csv
+        it = iter(out)
+        data = list(zip(it, it))
+        for index, tuple_ in enumerate(data):
+            values.append(tuple_)
+
+    if len(comuni) == 390 and len(values) == 390:
+        vax_tuples = []
+        for comune, (prima_dose, seconda_dose) in zip(comuni, values):
+            vax_tuples.append((comune, prima_dose, seconda_dose))
+    else:
+        print('nope')
+        exit()
+
+    vax = pd.DataFrame(vax_tuples, columns=[
+                       'comune', 'prima_dose', 'seconda_dose'])
+    out = pd.merge(vax, comuni_siciliani, on='comune', how='inner')
+    out = out[['cod_prov', 'pro_com_t', 'provincia',
+               'comune', 'prima_dose', 'seconda_dose']]
+    out.insert(0, 'data', date)
+    assert (len(out) == 390), "Errore: Sono presenti meno comuni del previsto."
+    # print(out)
+    lastUpdate = open(
+        path+'/dati/vaccini/vaccini.csv').read().rsplit('\n', 2)[1].split(',')[0]
+    if date != lastUpdate:
+        print('Esporto CSV...')
+        out.to_csv(path+'/dati/vaccini/vaccini-' +
+                   date.replace("-", "")+'.csv', index=None, header=True)
+        out.to_csv(path+'/dati/vaccini/vaccini-latest.csv',
+                   index=None, header=True)
+        out.to_csv(path+'/dati/vaccini/vaccini.csv',
+                   mode='a', index=None, header=False)
+    csv = path+'/dati/vaccini/vaccini-'+date.replace("-", "")+'.csv'
+    return csv
 
 
 pages = getPages('./download/'+latest['nome_file'])
-#getIncidenza('./download/'+latest['nome_file'], pages['incidenza'])
-#getVax('./download/'+latest['nome_file'], pages['vaccini'])
+getIncidenza('./download/'+latest['nome_file'], pages['incidenza'])
+getVax('./download/'+latest['nome_file'], pages['vaccini'])
