a474ac26f8575a39db3f3284c02a622897e4003a
Gabriele Scalici <gaba41@gmail.com>
2022-05-12 11:18:11 +0200

    continue refactoring, linting

--- scripts/convert_v2.py
+++ scripts/convert_v2.py
@@ -16,9 +16,7 @@ PATH_INCIDENCE = f"{PATH}/dati/incidenza/incidenza"
 PATH_VACCINES = f"{PATH}/dati/vaccini/vaccini"
 COMUNI_SICILIANI = "https://raw.githubusercontent.com/gabacode/vaxExtract/main/utilities/Elenco-comuni-siciliani.csv"
 
-comuni_siciliani = pd.DataFrame(
-    pd.read_csv(COMUNI_SICILIANI, converters={"pro_com_t": "{:0>6}".format})
-)
+comuni_siciliani = pd.DataFrame(pd.read_csv(COMUNI_SICILIANI, converters={"pro_com_t": "{:0>6}".format}))
 latest_report = pd.DataFrame(pd.read_csv(f"{PATH}/download/report.csv")).iloc[-1]
 latest_date = latest_report["data_report"]
 latest_file = latest_report["nome_file"]
@@ -27,16 +25,16 @@ latest_url = latest_report["URL"]
 
 def get_pages(file):
     """Get pages of interest from a PDF file"""
-    book = []
+    allegati = []
     pdf_file = fitz.open(file)
     print("Determino pagine da leggere...")
     for page in range(len(pdf_file)):
         pdf_page = pdf_file.load_page(page)
         pdf_text = pdf_page.get_text()
         if "ALLEGATO" in pdf_text:
-            book.append(page)
-    incidence_pages = list(range(book[0], book[1] + 1))
-    vaccines_pages = list(range(book[1] + 1, len(pdf_file) + 1))
+            allegati.append(page)
+    incidence_pages = list(range(allegati[0], allegati[1]))
+    vaccines_pages = list(range(allegati[1] + 1, len(pdf_file) + 1))
     output = {
         "data": latest_date,
         "incidenza": incidence_pages,
@@ -71,18 +69,11 @@ def add_to_readme():
             if '.pdf">Report' in line:
                 readme_indexes.append(index)
         insert_index = (readme_indexes[-1]) + 1
-        last_day = (
-            readme_lines[insert_index - 1]
-            .rpartition("Report ")[2]
-            .rpartition(" ")[0]
-            .rpartition(" ")[0]
-        )
+        last_day = readme_lines[insert_index - 1].rpartition("Report ")[2].rpartition(" ")[0].rpartition(" ")[0]
         new_day = latest_date.split("-")[2]
 
         if last_day != new_day:
-            insert_content = (
-                f'<li><a href="{latest_url}">Report {format_date}.pdf</a></li>\n'
-            )
+            insert_content = f'<li><a href="{latest_url}">Report {format_date}.pdf</a></li>\n'
             readme_lines.insert(insert_index, insert_content)
             readme.seek(0)
             readme.writelines(readme_lines)
@@ -134,7 +125,8 @@ def get_incidence(file, input_pages):
     print("Leggo tabella Incidenza...attendi...")
     pdf = fitz.open(file)
     all_text = []
-    for index in range(input_pages[0], input_pages[-1]):
+
+    for index in input_pages:
         page = pdf.load_page(index)
         text = page.get_text()
         text = text.replace("\n", " ")
@@ -173,9 +165,7 @@ def get_incidence(file, input_pages):
     data = list(zip(iter_list, iter_list, iter_list, iter_list))
 
     # Crea dataframe
-    dataframe = pd.DataFrame(
-        data, columns=["comune", "casi", "incidenza", "variazione"]
-    )
+    dataframe = pd.DataFrame(data, columns=["comune", "casi", "incidenza", "variazione"])
     dataframe = dataframe[["comune", "incidenza", "casi"]]
 
     # Rimuovi distretti (duplicati)
@@ -191,39 +181,14 @@ def get_incidence(file, input_pages):
         how="inner",
     )
     output.rename(columns={"comune_y": "comune"}, inplace=True)
-    output = output[
-        ["cod_prov", "pro_com_t", "provincia", "comune", "incidenza", "casi"]
-    ].sort_values(by=["provincia", "comune"])
+    output = output[["cod_prov", "pro_com_t", "provincia", "comune", "incidenza", "casi"]].sort_values(
+        by=["provincia", "comune"]
+    )
     output.reset_index(drop=True, inplace=True)
     output.insert(0, "data", latest_date)
 
-    # Controlla che ci siano tutti i comuni
     assert len(output) == 390, "Errore: Sono presenti meno comuni del previsto."
-
-    # Esporta CSV
-    last_update = (
-        open(f"{PATH_INCIDENCE}.csv", encoding="UTF-8")
-        .read()
-        .rsplit("\n", 2)[1]
-        .split(",")[0]
-    )
-    if latest_date != last_update:
-        print("Esporto CSV...")
-        format_date = latest_date.replace("-", "")
-        output.to_csv(
-            f"{PATH_INCIDENCE}-{format_date}.csv",
-            index=None,
-            header=True,
-        )
-        output.to_csv(f"{PATH_INCIDENCE}-latest.csv", index=None, header=True)
-        output.to_csv(
-            f"{PATH_INCIDENCE}.csv",
-            mode="a",
-            index=None,
-            header=False,
-        )
-    format_date = latest_date.replace("-", "")
-    return f"{PATH_INCIDENCE}-{format_date}.csv"
+    return export_csv(output, PATH_INCIDENCE)
 
 
 def get_vaccines(file, input_pages):
@@ -234,14 +199,13 @@ def get_vaccines(file, input_pages):
     letters_config = r"--oem 3 --psm 12 -c tessedit_char_blacklist=[]()!"
     numbers_config = r"--oem 3 --psm 12 -c tessedit_char_whitelist=0123456789,%"
     vaccini_latest = f"{PATH_VACCINES}-latest.csv"
-    report = pd.DataFrame(
-        pd.read_csv(vaccini_latest, converters={"pro_com_t": "{:0>6}".format})
-    )
+    report = pd.DataFrame(pd.read_csv(vaccini_latest, converters={"pro_com_t": "{:0>6}".format}))
     comuni = []
     values = []
     lista_comuni = report["comune"].tolist()
     pdf = fitz.open(file)
     vaccini_pages = list(range(input_pages[0], input_pages[-1]))
+
     for index in vaccini_pages:
         page = pdf.load_page(index)
         pixmap = page.get_pixmap(alpha=False, matrix=matrix).tobytes()
@@ -252,9 +216,7 @@ def get_vaccines(file, input_pages):
         comuni_column = resized[730 : image.height * 2, 230:620]
         comuni_column_image = Image.fromarray(comuni_column)
         processed_comuni = prepare_image(comuni_column_image, "names")
-        results = pytesseract.image_to_string(
-            processed_comuni, config=letters_config
-        ).split("\n")
+        results = pytesseract.image_to_string(processed_comuni, config=letters_config).split("\n")
         lista_risultati = [i for i in results if i]
         lista_risultati.remove("\x0c")
         for word in lista_risultati:
@@ -263,15 +225,15 @@ def get_vaccines(file, input_pages):
                     comuni.append(comune)
                     del lista_comuni[i]
                     break
-                elif similar(word, comune) > 0.85:
+                if similar(word, comune) > 0.85:
                     del lista_comuni[i]
                     comuni.append(comune)
                     break
-                elif similar(word, comune) > 0.66:
+                if similar(word, comune) > 0.66:
                     del lista_comuni[i]
                     comuni.append(comune)
                     break
-                elif similar(word, comune) > 0.57:
+                if similar(word, comune) > 0.57:
                     del lista_comuni[i]
                     comuni.append(comune)
                     break
@@ -282,9 +244,7 @@ def get_vaccines(file, input_pages):
         numbers_column = resized[730 : image.height * 2, 1500:1900]
         numbers_column_image = Image.fromarray(numbers_column)
         processed_numbers = prepare_image(numbers_column_image, "numbers")
-        results = pytesseract.image_to_string(
-            processed_numbers, config=numbers_config
-        ).split("\n")
+        results = pytesseract.image_to_string(processed_numbers, config=numbers_config).split("\n")
 
         out = [i for i in results if i]
         out.remove("\x0c")
@@ -297,38 +257,37 @@ def get_vaccines(file, input_pages):
             values.append(vax_tuple)
 
     if len(comuni) == 390 and len(values) == 390:
-        vax_tuples = []
+        vax_rows = []
         for comune, (prima_dose, seconda_dose) in zip(comuni, values):
-            vax_tuples.append((comune, prima_dose, seconda_dose))
+            vax_row = (comune, prima_dose, seconda_dose)
+            vax_rows.append(vax_row)
     else:
-        print("nope")
-        print(len(comuni), len(values))
-        exit()
+        raise ValueError("Errore: Sono presenti meno comuni del previsto.", len(comuni), len(values))
 
-    vax = pd.DataFrame(vax_tuples, columns=["comune", "prima_dose", "seconda_dose"])
+    vax = pd.DataFrame(vax_rows, columns=["comune", "prima_dose", "seconda_dose"])
     output = pd.merge(vax, comuni_siciliani, on="comune", how="inner")
-    output = output[
-        ["cod_prov", "pro_com_t", "provincia", "comune", "prima_dose", "seconda_dose"]
-    ]
+    output = output[["cod_prov", "pro_com_t", "provincia", "comune", "prima_dose", "seconda_dose"]]
     output.insert(0, "data", latest_date)
     assert len(output) == 390, "Errore: Sono presenti meno comuni del previsto."
-    last_update = (
-        open(f"{PATH_VACCINES}.csv", encoding="UTF-8")
-        .read()
-        .rsplit("\n", 2)[1]
-        .split(",")[0]
-    )
-    if latest_date != last_update:
+    return export_csv(output, PATH_VACCINES)
+
+
+def export_csv(data, path):
+    """Export data to CSV"""
+    with open(f"{path}-latest.csv", "r", encoding="utf-8") as last_update:
+        last_update_date = last_update.readlines()[1].split(",")[0]
+    if latest_date == last_update_date:
         print("Esporto CSV...")
         format_date = latest_date.replace("-", "")
-        output.to_csv(
-            f"{PATH_VACCINES}-{format_date}.csv",
+        data.to_csv(
+            f"{path}-{format_date}.csv",
             index=None,
             header=True,
         )
-        output.to_csv(f"{PATH_VACCINES}-latest.csv", index=None, header=True)
-        output.to_csv(f"{PATH_VACCINES}.csv", mode="a", index=None, header=False)
-    return f"{PATH_VACCINES}-{format_date}.csv"
+        data.to_csv(f"{path}-latest.csv", index=None, header=True)
+        data.to_csv(f"{path}.csv", mode="a", index=None, header=False)
+    else:
+        print("Dati già esistenti, non esporto CSV...")
 
 
 # Function calls
